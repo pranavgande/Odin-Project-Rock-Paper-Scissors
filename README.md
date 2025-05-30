cca0e64ef01b05a7533f969769b297f394065566
pranavgande <186005448+pranavgande@users.noreply.github.com>
2025-05-30 04:43:19 +0000

    addded the buttons for choices

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Odin-Project-Rock-Paper-Scissors
+I'm trying to make a rock paper scissors game using basic javascript.
--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("Hello World")
+console.log("Hello World");
+function getComputerChoice(){
+    const choices = ["rock", "paper", "scissor"];
+    const randomindex = Math.floor(Math.random() * choices.length);
+    return choices[randomindex]
+}
+const signbutton = document.querySelector("#signbutton");
+
+function getHumanChoice(){
+    signbutton.addEventListener("click", () =>{
+    let sign = prompt("Stone, Paper or Scissor?").toLowerCase();
+    const computerchoice = getComputerChoice();
+    console.log("You chose:", sign);
+    console.log("Computer chose:", computerchoice);
+    
+
+    if (sign == computerchoice){
+        return ("its a tie");
+    }else if(
+    (sign == stone && computerchoice == scissor)||
+    (sign == paper && computerchoice == stone)||
+    (sign == scissor && computerchoice == paper)
+    )
+    {
+        console.log("You Win!");
+    }else
+    {
+        console.log("You lose");
+    }
+})
+}
+getComputerChoice();
